0221037519b4f8297035195f88a2cb4b639861f1
dvrzan <dvrzan@icloud.com>
2020-04-03 13:17:33 -0400

    major ui refactor

---
+++ Alarme/Views/AlarmTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmTableViewCell.swift
+//  Alarme
+//
+//  Created by Danijela Vrzan on 2020-04-03.
+//  Copyright © 2020 Danijela Vrzan. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
